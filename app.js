b20d5f2a326f217f690482a70dacf50273625b3c
Aditya <adityarpadekar@gmail.com>
2023-08-27 13:59:53 +0530

    vercel test 1

--- app.js
+++ app.js
@@ -1,41 +1,36 @@
-const express = require('express')
-require('dotenv').config()
-const notFoundMW = require('./middleware/not-found')
-const errorHandler = require('./middleware/error-handler')
-const connectDB = require('./db/connect')
-const products = require('./routes/products')
-require('express-async-errors')
+const express = require("express");
+require("dotenv").config();
+const notFoundMW = require("./middleware/not-found");
+const errorHandler = require("./middleware/error-handler");
+const connectDB = require("./db/connect");
+const products = require("./routes/products");
+require("express-async-errors");
 
-const app = express()
+const app = express();
 
+app.use(express.json());
 
-app.use(express.json())
+// app.get("/", (req, res) => {
+//     res.send('<h1>STORE API</h1><a href="/api/v1/products">products</a>');
+// });
 
-app.get('/', (req, res) => {
-    res.send('<h1>STORE API</h1><a href="/api/v1/products">products</a>')
-})
+app.use("/", products);
 
-app.use('/api/v1/products', products)
-
-
-
-app.use(notFoundMW)
-app.use(errorHandler)
-
-
-
-
+app.use(notFoundMW);
+app.use(errorHandler);
 
 //listen
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080;
 
 const startServer = async () => {
     try {
-        await connectDB(process.env.MONGO_URI)
-        app.listen(port, () => { console.log(`Server is listening on port ${port}`) })
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}`);
+        });
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
-}
+};
 
-startServer()
+startServer();
